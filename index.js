12ede0b7cc645286a6c4dc9989734d9d2c63b04b
teawithsand <teawithsand@gmail.com>
2025-05-21 08:59:43 +0200

    test 1 (#1)
    * test 1
    
    * fix1
    
    * fix2
    
    * version bump
    
    * fix3

--- index.js
+++ index.js
@@ -11,23 +11,23 @@ const main = async () => {
     const baseSha = github.context.payload.pull_request.base.sha;
     const headSha = github.context.payload.pull_request.head.sha;
 
-    const baseUrl = `https://raw.githubusercontent.com/${ github.context.repo.owner }/${ github.context.repo.repo }/${ baseSha }/${ packageJsonPath }`
+    const baseUrl = `https://raw.githubusercontent.com/${ github.context.repo.owner }/${ github.context.repo.repo }/${ baseSha }/version.txt`
 
     const localVersionRaw = fs.readFileSync(`${ process.env.GITHUB_WORKSPACE }/version.txt`)
     const localVersion = parseInt(localVersionRaw);
 
-    const res = await fetch(baseUrl, { headers })
+    const res = await fetch(baseUrl)
     const versionRaw = await res.text()
-    const version = parseInt(versionRaw);
+    const baseVersion = parseInt(versionRaw);
 
     console.log("Comparing", {
         baseSha,
         headSha,
-        targetVersion: version,
+        baseVersion: baseVersion,
         localVersion: localVersion,
     })
 
-    if (version <= localVersion) {
+    if (localVersion <= baseVersion) {
         throw new Error("Version mismatch: Expected version to be higher.")
     }
 }
